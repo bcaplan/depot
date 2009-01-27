5ebfafdd964673f9f488a908b69bebded5311460
Brandon Caplan <brandon@bcap-mbp.local>
2009-01-26 21:47:12 -0800

    Added the store and made all tests pass

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ActiveRecord::Base
+  
+  def self.find_products_for_sale 
+    find(:all, :order => "title") 
+  end
+  
   validates_presence_of :title, :description, :image_url
   validates_numericality_of :price
   validate :price_must_be_at_least_a_cent
@@ -6,7 +11,7 @@ class Product < ActiveRecord::Base
   validates_format_of :image_url,
                       :with    => %r{\.(gif|jpg|png)$}i,
                       :message => 'must be a URL for a GIF, JPG, or PNG image.'
-  
+
   protected
     def price_must_be_at_least_a_cent
       errors.add(:price, 'should be at least 0.01') if price.nil? || price < 0.01
