2fde186765824fa02df0d063b326565fb5a9c002
Brandon Caplan <brandon@bcap-mbp.local>
2009-01-26 20:04:23 -0800

    Updated product model with validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,15 @@
 class Product < ActiveRecord::Base
-  validates_presence_of :title
+  validates_presence_of :title, :description, :image_url
+  validates_numericality_of :price
+  validate :price_must_be_at_least_a_cent
+  validates_uniqueness_of :title
+  validates_format_of :image_url,
+                      :with    => %r{\.(gif|jpg|png)$}i,
+                      :message => 'must be a URL for a GIF, JPG, or PNG image.'
+  
+  protected
+    def price_must_be_at_least_a_cent
+      errors.add(:price, 'should be at least 0.01') if price.nil? || price < 0.01
+      
+    end
 end
