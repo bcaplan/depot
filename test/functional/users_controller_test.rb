8183907c210bcf436e5701b7d0dc230d82c336a9
Brandon Caplan <brandon@bcap-mbp.local>
2009-02-12 20:52:56 -0800

    Added users. Set up hashed password. Made tests pass.

---
+++ test/functional/users_controller_test.rb
@@ -0,0 +1,52 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:users)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create user" do
+    assert_difference('User.count') do
+      post :create, :user => {
+          :name             => 'John Doe',
+          :hashed_password  => User.encrypted_password('fake', 'person'),
+          :salt             => 'person'
+        }
+    end
+    
+    assert flash[:notice]
+    assert_redirected_to :controller => 'users', :action => 'index'
+  end
+
+  test "should show user" do
+    get :show, :id => users(:one).id
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, :id => users(:one).id
+    assert_response :success
+  end
+
+  test "should update user" do
+    put :update, :id => users(:one).id, :user => { }
+    
+    assert flash[:notice]
+    assert_redirected_to :controller => 'users', :action => 'index'
+  end
+
+  test "should destroy user" do
+    assert_difference('User.count', -1) do
+      delete :destroy, :id => users(:one).id
+    end
+
+    assert_redirected_to users_path
+  end
+end
