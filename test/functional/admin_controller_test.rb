6859897e611b736dd18bf53e1322a9132c998fa4
Brandon Caplan <brandon@bcap-mbp.local>
2009-02-14 14:32:10 -0800

    Added admin controller tests

--- test/functional/admin_controller_test.rb
+++ test/functional/admin_controller_test.rb
@@ -1,8 +1,40 @@
 require 'test_helper'
 
 class AdminControllerTest < ActionController::TestCase
-  def setup
-    @request.session[:user_id] = users(:one).id
+  
+  test "logging in" do
+    post :login, :name => 'Cookie Monster', :password => 'cookie'
+    assert_redirected_to :action => "index"
   end
- 
+  
+  test "logging in takes you to original destination" do
+    @request.session[:original_uri] = "/products"
+    post :login, :name => 'Cookie Monster', :password => 'cookie'
+    assert_redirected_to :controller => "products", :action => "index"
+  end
+  
+  test "logging out" do
+    @request.session[:user_id] = users(:one).id
+    post :logout
+    assert @request.session[:user_id] == nil
+  end
+  
+  test "cannot log in with bad password" do
+    post :login, :name => 'Cookie Monster', :password => 'doughnut'
+    assert_response :success
+    assert @request.session[:user_id] == nil
+    assert flash[:notice]
+  end
+  
+  test "cannot log in with bad user" do
+    post :login, :name => 'Anonymous', :password => 'canhazaccess?'
+    assert_response :success
+    assert @request.session[:user_id] == nil
+    assert flash[:notice]
+  end
+  
+  test "cannot access admin unless logged in" do
+    get :index
+    assert_redirected_to :action => "login"
+  end
 end
