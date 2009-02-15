c62dd16df945769f0326d5ba58a744113f1c3e9c
Brandon Caplan <brandon@bcap-mbp.local>
2009-02-14 18:37:52 -0800

    Added user model tests

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "raises when deleting last user" do
+    User.first.destroy
+    assert_raise RuntimeError do  
+      User.first.destroy
+    end
+  end
+  
+  test "authentication with good info" do
+    assert_not_nil User.authenticate 'Cookie Monster', 'cookie'
+  end
+  
+  test "authentication with bad info" do
+    assert_nil User.authenticate 'Person', 'password'
   end
 end
