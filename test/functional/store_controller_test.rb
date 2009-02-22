0fc1a0b3162ba6408cd31a5eef7b700a9d6ae69b
Brandon Caplan <brandon@bcap-mbp.local>
2009-02-21 19:06:47 -0800

    Added i18n tests

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -2,6 +2,23 @@ require 'test_helper'
 
 class StoreControllerTest < ActionController::TestCase
   
+  test "default locale is used when locale is not in session" do
+    @request.session[:locale] = nil
+    get :index
+    
+    assert_equal I18n.default_locale, I18n.locale
+  end
+  
+  test "store controller is localized" do
+       locale = LANGUAGES.to_a.first.last
+       @request.session[:user_id] = users(:one).id
+       get :index, :locale => locale
+
+       translations = YAML.load_file("#{LOCALES_DIRECTORY}#{locale}.yml")
+
+       assert_match translations[locale]['layout']['side']['questions'], @response.body
+  end
+  
   test "checkout redirects if cart empty" do
     @request.session[:cart] = nil
     post :checkout
