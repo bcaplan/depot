7f37ad23b6f41798a538d5ab5a2705b069914cf9
Brandon Caplan <brandon@bcap-mbp.local>
2009-02-21 17:53:02 -0800

    Added tests for who_bought in HTML, XML, ATOM, and JSON

--- test/functional/info_controller_test.rb
+++ test/functional/info_controller_test.rb
@@ -1,8 +1,34 @@
 require 'test_helper'
 
 class InfoControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "info gets xml" do
+    get :who_bought, :id => products(:one).id, :format => 'xml'
+    
+    assert_response :success
+    assert_tag :tag => 'title', :content => products(:one).title
+  end
+  
+  test "info gets atom" do
+    get :who_bought, :id => products(:one).id, :format => 'atom'
+    
+    assert_response :success
+    assert_tag :tag => 'title', :content => "Who bought #{products(:one).title}"
+  end
+  
+  test "info gets json" do
+    get :who_bought, :id => products(:one).id, :format => 'json'
+    
+    assert_response :success
+    doc = ActiveSupport::JSON.decode(@response.body)
+    
+    assert_equal doc['product']['title'], products(:one).title
+  end
+  
+  test "info gets html" do
+    get :who_bought, :id => products(:one).id
+    
+    assert_response :success
+    assert_tag :tag => 'div', :attributes => { :id => 'main' },
+               :descendant => { :tag => 'h3', :content => "People Who Bought #{products(:one).title}" }
   end
 end
